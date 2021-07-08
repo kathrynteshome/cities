7085626869c14b4cfa3556628ba5e6a65c9dbc04
kathryncgi <kathryn.teshome@cgi.com>
2021-07-08 15:31:53 -0400

    made all tests pass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [ExperiencesService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
